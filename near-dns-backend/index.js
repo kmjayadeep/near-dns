af69f0e5638c2d5388eba9e338a259656706b6f4
Jayadeep KM <kmjayadeep@gmail.com>
2025-01-10 22:16:03 +0100

    change cronjob to deploy
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- near-dns-backend/index.js
+++ near-dns-backend/index.js
@@ -19,112 +19,120 @@ const near = await connect(connectionConfig);
 const contractId = "near-dns-test2.testnet";
 const methodName = "get_all_domains";
 
-try {
-    const result = await near.connection.provider.query({
-        request_type: "call_function",
-        account_id: contractId,
-        method_name: methodName,
-        args_base64: "",
-        finality: "final",
-    });
+async function run() {
+  try {
+      const result = await near.connection.provider.query({
+          request_type: "call_function",
+          account_id: contractId,
+          method_name: methodName,
+          args_base64: "",
+          finality: "final",
+      });
 
-    const records = JSON.parse(Buffer.from(result.result).toString());
-    processRecords(records)
-} catch (error) {
-    console.error("Error calling read-only method:", error);
+      const records = JSON.parse(Buffer.from(result.result).toString());
+      processRecords(records)
+  } catch (error) {
+      console.error("Error calling read-only method:", error);
+  }
 }
 
-
 async function processRecords(records) {
-    const existing = await listRecords()
+  const existing = await listRecords()
 
-    for(let i=0;i<records.length; i++){
-        const record = records[i]
-        const domain = `${record[0]}.local`
-        const { A, AAAA } = record[1]
+  for(let i=0;i<records.length; i++){
+    const record = records[i]
+    const domain = `${record[0]}.local`
+    const { A, AAAA } = record[1]
 
-        if(!existing[domain]) {
-            await addRecord(domain, A, AAAA)
-        }else{
-            if(existing[domain].answer != A ) {
-                await updateRecord(existing[domain], domain, A, AAAA)
-            }
-        }
-
+    if(!existing[domain]) {
+      await addRecord(domain, A, AAAA)
+    }else{
+      if(existing[domain].answer != A ) {
+        await updateRecord(existing[domain], domain, A, AAAA)
+      }
     }
+
+  }
 }
 
 async function listRecords() {
-    console.log('listing domains')
-    const url = `http://gatekeeper.cosmos.cboxlab.com/control/rewrite/list`;
-    const basicAuth = btoa(`admin:${ADGUARD_PASSWORD}`);
+  console.log('listing domains')
+  const url = `http://gatekeeper.cosmos.cboxlab.com/control/rewrite/list`;
+  const basicAuth = btoa(`admin:${ADGUARD_PASSWORD}`);
 
 
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Basic ${basicAuth}`
-        }
-    });
-    
-    const body = await response.json()
-    console.log(`got ${body.length} domains`)
-    const records = new Map()
-
-    for (const r of body) {
-        records[r.domain] = r
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Basic ${basicAuth}`
     }
+  });
 
-    return records
+  const body = await response.json()
+  console.log(`got ${body.length} domains`)
+  const records = new Map()
+
+  for (const r of body) {
+    records[r.domain] = r
+  }
+
+  return records
 }
 
 async function addRecord(domain, A, AAAA) {
-    console.log(`adding ${domain} with ${A}`)
-    const url = `http://gatekeeper.cosmos.cboxlab.com/control/rewrite/add`;
-    const basicAuth = btoa(`admin:${ADGUARD_PASSWORD}`);
+  console.log(`adding ${domain} with ${A}`)
+  const url = `http://gatekeeper.cosmos.cboxlab.com/control/rewrite/add`;
+  const basicAuth = btoa(`admin:${ADGUARD_PASSWORD}`);
 
 
-    const rewriteRule = {
-        domain,
-        answer: A,
-    };
+  const rewriteRule = {
+    domain,
+    answer: A,
+  };
 
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Basic ${basicAuth}`
-        },
-        body: JSON.stringify(rewriteRule)
-    });
-    if(response.ok) {
-        console.log("added successfully")
-    }
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Basic ${basicAuth}`
+    },
+    body: JSON.stringify(rewriteRule)
+  });
+  if(response.ok) {
+    console.log("added successfully")
+  }
 }
 
 async function updateRecord(record, domain, A, AAAA) {
-    console.log(`updating ${record} with ${A}`)
-    const url = `http://gatekeeper.cosmos.cboxlab.com/control/rewrite/update`;
-    const basicAuth = btoa(`admin:${ADGUARD_PASSWORD}`);
+  console.log(`updating ${record} with ${A}`)
+  const url = `http://gatekeeper.cosmos.cboxlab.com/control/rewrite/update`;
+  const basicAuth = btoa(`admin:${ADGUARD_PASSWORD}`);
 
-    const payload = {
-        target: record,
-        update: {
-            domain,
-            answer: A,
-        }
+  const payload = {
+    target: record,
+    update: {
+      domain,
+      answer: A,
     }
+  }
 
-    const response = await fetch(url, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Basic ${basicAuth}`
-        },
-        body: JSON.stringify(payload)
-    });
-    if(response.ok) {
-        console.log("updated successfully")
-    }
+  const response = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Basic ${basicAuth}`
+    },
+    body: JSON.stringify(payload)
+  });
+  if(response.ok) {
+    console.log("updated successfully")
+  }
+}
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+while(1) {
+  await run()
+  await sleep(60000)
 }
