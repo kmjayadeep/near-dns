6d58ff8681b62dd96f9d57a85f0fc4625c16b891
Jayadeep KM <kmjayadeep@gmail.com>
2025-01-30 18:13:08 +0000

    use a different testnet
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- near-dns-backend/index.js
+++ near-dns-backend/index.js
@@ -8,7 +8,7 @@ const { connect, keyStores } = nearAPI;
 const connectionConfig = {
   networkId: "testnet",
   keyStore: new keyStores.InMemoryKeyStore(),
-  nodeUrl: "https://test.rpc.fastnear.com",
+  nodeUrl: "https://archival-rpc.testnet.near.org",
   walletUrl: "https://testnet.mynearwallet.com/",
   helperUrl: "https://helper.testnet.near.org",
   explorerUrl: "https://testnet.nearblocks.io",
