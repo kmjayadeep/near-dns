a70a29f8a929d750e2ab0f508d5af614e11b3309
Jayadeep KM <kmjayadeep@gmail.com>
2025-02-08 09:56:55 +0100

    use staging for backend
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- near-dns-backend/src/main.rs
+++ near-dns-backend/src/main.rs
@@ -14,7 +14,7 @@ struct DNSRecord {
 async fn get_all_domains() -> Result<Vec<(String, DNSRecord)>, Box<dyn std::error::Error>> {
     let network = NetworkConfig::testnet();
 
-    let contract_id: AccountId = "near-dns-test3.testnet".parse().unwrap();
+    let contract_id: AccountId = "near-dns-staging.testnet".parse().unwrap();
 
     // Load the contract
     let contract = Contract(contract_id.clone());
